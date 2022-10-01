69d2182c80ed0697e391752dc23219343b64dddf
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-01 11:31:44 -0700

    set up higher level App component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+      I LOAD!
+    </div>)
+  }
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
-
 import React from 'react';
 import reactDom from 'react-dom';
+import App from './components/App.jsx';
 
 const Atelier = () => {
   return (
