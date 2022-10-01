b24da9084a7f32e370739c6540f8d532911ee81f
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-01 11:34:11 -0700

    Render the RelatedItems Component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RelatedItems from './relatedItems/RelatedItems.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,7 +8,7 @@ class App extends React.Component {
 
   render() {
     return (<div>
-      I LOAD!
+      <RelatedItems />
     </div>)
   }
 }
---
+++ client/src/components/relatedItems/RelatedItems.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RelatedItems extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>
+      <h1>Related Items and Comparison</h1>
+    </div>)
+  }
+}
+
+export default RelatedItems;
