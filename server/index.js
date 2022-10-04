e33e648384cd8b02d9ca1f0360bab239d249b610
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-03 19:44:02 -0700

    add new middleware functions

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 require("dotenv").config();
 
 const express = require('express');
+const logger = require("./middleware/logger");
+const auth = require("./middleware/auth");
+
+
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use(logger);
+app.use(auth);
+
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(port, () => {
