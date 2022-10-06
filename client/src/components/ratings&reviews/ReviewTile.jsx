063591a031f29761441bdf632fcd533bd27c2c11
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-10-05 23:46:26 -0400

    rendering data for all reviews for a static product ID for now

--- client/src/components/ratings&reviews/ReviewTile.jsx
+++ client/src/components/ratings&reviews/ReviewTile.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import stars from '../helpers/stars.js';
 import axios from 'axios';
 
-class ReviewTile extends React.Component{
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <div>{stars(3.75)}</div>
-        <h3> Review Summary </h3>
-        <h4> Review Body </h4>
-      </div>
-    )
-  }
+const ReviewTile = ({ review }) => {
+  return (
+    <div>
+      <div>{stars(review.rating)}</div>
+      <h3> {review.summary} </h3>
+      <h4> {review.body} </h4>
+  </div>
+  )
 }
 
 export default ReviewTile;
--- client/src/components/ratings&reviews/ReviewsList.jsx
+++ client/src/components/ratings&reviews/ReviewsList.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReviewTile from './ReviewTile.jsx'
+import axios from 'axios';
 
 
 class ReviewsList extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      reviews: []
+    }
+
+    this.getReviews = this.getReviews.bind(this);
+  }
+
+  componentDidMount() {
+    this.getReviews();
+  }
+
+  getReviews() {
+    axios({
+      method: 'get',
+      url: '/reviews/',
+      params: {product_id: 72000, count: 100},
+    })
+    .then(reviews => {
+      console.log(reviews.data.results);
+      this.setState({
+        reviews: reviews.data.results
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   render() {
     return (
       <div>
         <h3>An amount of reviews to be sorted</h3>
-        <ReviewTile />
+        {this.state.reviews.map((review) => {
+          return (
+            <ReviewTile review={review} key={review.review_id} />
+          )
+        })}
       </div>
     )
   }
