a7d72b0f4b656d0652740e528dc8729a059f5ddf
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-05 17:31:53 -0700

    pull product and rating information

--- client/src/components/relatedItems/ProductCard.jsx
+++ client/src/components/relatedItems/ProductCard.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import ComparisonModal from './ComparisonModal.jsx';
-
+const axios = require('axios');
 
 class ProductCard extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productId: 71698,
+      productCategory: '',
+      productName: '',
+      originalPrice: 0,
+      starRating: 0,
+      numRatings: 0,
+      isOnSale: false,
+      salesPrice: 0
+    }
+  }
+
+  componentDidMount() {
+    var productCategory, productName, originalPrice, isOnSale, salesPrice;
+    var starRating = 0;
+    var numRatings = 0;
+
+    axios({
+      method: 'get',
+      url: `/products/${this.state.productId}`,
+      params: {product_id: this.state.productId}
+    })
+    .then(result => {
+      productCategory = result.data.category;
+      productName = result.data.name;
+      originalPrice = result.data.default_price;
+
+      return axios({
+        method: 'get',
+        url: `/reviews/meta`,
+        params: {product_id: this.state.productId}
+      })
+    })
+    .then(result => {
+      var ratings = result.data.ratings;
+
+      for(var i = 1; i <= 5; i++) {
+        starRating += i * parseInt(ratings[i], 10);
+        numRatings += parseInt(ratings[i], 10)
+      }
+
+      starRating = starRating / numRatings;
+
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   render() {
