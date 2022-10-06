f49e0d2a21864ffcafb075a1730b3f793fe3c16b
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-10-05 23:44:29 -0400

    moved axios api call to ReviewsList

--- client/src/components/ratings&reviews/RatingsReviews.jsx
+++ client/src/components/ratings&reviews/RatingsReviews.jsx
@@ -6,30 +6,7 @@ import axios from 'axios';
 class RatingsReviews extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {
-      reviews: []
-    }
-
-    // this.getReviews = this.getReviews.bind(this);
   }
-
-  // componentDidMount() {
-  //   this.getReviews();
-  // }
-
-  // getReviews() {
-  //   axios({
-  //     method: 'get',
-  //     url: '/reviews',
-  //     params: {product_id: 2, count: 10},
-  //   })
-  //   .then(result => {
-  //     console.log(result);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  // }
 
   render() {
     return (
