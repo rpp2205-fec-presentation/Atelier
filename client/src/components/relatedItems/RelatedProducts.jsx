6bbf57bee0df936d0ba258e0830ab850ceba6db4
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-07 12:51:11 -0700

    dynamically pull related items and display them

--- client/src/components/relatedItems/RelatedProducts.jsx
+++ client/src/components/relatedItems/RelatedProducts.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import ProductCard from './ProductCard.jsx';
+const axios = require('axios');
 
 class RelatedProducts extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productId: 71697,
+      relatedProducts: [71697, 71698, 71699]
+    }
+
+    this.getRelatedProducts = this.getRelatedProducts.bind(this);
+  }
+
+  getRelatedProducts(productId) {
+    axios({
+      method: 'get',
+      url: `/products/${productId}/related`,
+      params: {product_id: productId}
+    })
+    .then(result => {
+      this.setState({relatedProducts: result.data})
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  componentDidMount() {
+    this.getRelatedProducts(this.state.productId);
   }
 
   render() {
     return (<div id='related-products'>
       <h3>Related Products</h3>
-      <ProductCard />
-      <ProductCard />
+      {this.state.relatedProducts.map((productId) =>
+        <ProductCard key={productId.toString()} productId={productId}/>)}
     </div>)
   }
 }
