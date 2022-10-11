9405eed6efc4c54bb21fcc088c8e20e42b60a117
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-10-10 20:02:21 -0400

    added file for calculating the bar scales for characteritics, rendering characteristics

--- client/src/components/ratings&reviews/RatingsBD.jsx
+++ client/src/components/ratings&reviews/RatingsBD.jsx
@@ -3,6 +3,12 @@ import axios from 'axios';
 import styled from 'styled-components';
 import calculateAverageRating from '../helpers/calculateAverageRating.js';
 import stars from '../helpers/stars.js';
+import PrctScaleChar from './PrctScaleChar.jsx';
+
+const ScaleText = styled.span`
+  float: right;
+  padding: 0 3px;
+`;
 
 const RatingsBD = ({ productId, metaData, ratings }) => {
 
@@ -19,6 +25,70 @@ const RatingsBD = ({ productId, metaData, ratings }) => {
     return (count === 0) ? 5 : (total / count).toFixed(1);
   };
 
+  let char = metaData.characteristics;
+  if (char !== undefined) {
+    var size = char.Size || '';
+    var width = char.Width || '';
+    var comfort = char.Comfort || '';
+    var quality = char.Quality || '';
+    var length = char.Length || '';
+    var fit = char.Fit || '';
+  }
+
+  let charBreakdown = (
+    char !== undefined ?
+      <>
+        {!!size && (
+          <div>
+            <span><b>Size </b></span> <br />
+            <PrctScaleChar value={Number(size.value).toFixed(1)} />
+            <span>a size too small</span><ScaleText>a size too big</ScaleText> <br/>
+          </div>
+        )}
+          <br></br>
+        {!!width && (
+          <div>
+            <span><b>Width </b></span> <br />
+            <PrctScaleChar value={Number(width.value).toFixed(1)} />
+            <span>too narrow</span><ScaleText>too wide</ScaleText> <br/>
+          </div>
+        )}
+        <br></br>
+        {!!comfort && (
+          <div>
+            <span><b>Comfort </b></span> <br />
+            <PrctScaleChar value={Number(comfort.value).toFixed(1)} />
+            <span>uncomfortable</span><ScaleText>perfect</ScaleText> <br/>
+          </div>
+        )}
+        <br></br>
+        {!!quality && (
+          <div>
+            <span><b>Quality </b></span> <br />
+            <PrctScaleChar value={Number(quality.value).toFixed(1)} />
+            <span>poor</span><ScaleText>perfect</ScaleText> <br/>
+          </div>
+        )}
+        <br></br>
+        {!!length && (
+          <div>
+            <span><b>Length </b></span> <br />
+            <PrctScaleChar value={Number(length.value).toFixed(1)} />
+            <span>runs short</span><ScaleText>runs long</ScaleText> <br/>
+          </div>
+        )}
+        <br></br>
+        {!!fit && (
+          <div>
+            <span><b>Fit </b></span> <br />
+            <PrctScaleChar value={Number(fit.value).toFixed(1)} />
+            <span>runs tight</span><ScaleText>runs big</ScaleText> <br/>
+          </div>)}
+      </> :
+      <></>
+  );
+
+
         return (
           <div>
             <h3> Rating Breakdown </h3>
@@ -26,10 +96,11 @@ const RatingsBD = ({ productId, metaData, ratings }) => {
             <h4>{avgRating(ratings)}</h4>
             <h4>{stars(avgRating(ratings))}</h4>
             <h2>Ratings By Stars</h2>
-            <ul>{Object.keys(theRatings).map(rating => {
-                  return <li key={rating}>{rating}: {theRatings[rating]}</li>;
+            <div>{Object.keys(theRatings).map(rating => {
+                  return <li key={rating}>{stars(rating)}: {theRatings[rating]}</li>;
                 })}
-            </ul>
+            </div>
+            {charBreakdown}
           </div>
         )
 
