4a433b37b800302f63c91d5015b35cc80646170e
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-12 19:29:30 -0700

    add in all necessary mock servers to avoid error warnings

--- test/related-items-tests/ProductCard.test.js
+++ test/related-items-tests/ProductCard.test.js
@@ -40,6 +40,72 @@ const server = setupServer(
       5: 3
     }}))
   }),
+  rest.get('/products/:product_id/styles', (req, res, ctx) => {
+    // respond using a mocked JSON body
+    return res(ctx.json({
+      "product_id": "1",
+      "results": [
+      {
+              "style_id": 1,
+              "name": "Forest Green & Black",
+              "original_price": "140",
+              "sale_price": "0",
+              "default?": true,
+              "photos": [
+          {
+                      "thumbnail_url": "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+                      "url": "urlplaceholder/style_1_photo_number.jpg"
+                  },
+          {
+                      "thumbnail_url": "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+                      "url": "urlplaceholder/style_1_photo_number.jpg"
+                  }
+              ],
+          "skus": {
+                    "37": {
+                          "quantity": 8,
+                          "size": "XS"
+                    },
+                    "38": {
+                          "quantity": 16,
+                          "size": "S"
+                    },
+                    "39": {
+                          "quantity": 17,
+                          "size": "M"
+                    },
+                }
+      },
+    {
+          "style_id": 2,
+          "name": "Desert Brown & Tan",
+          "original_price": "140",
+          "sale_price": "0",
+          "default?": false,
+          "photos": [
+          {
+                      "thumbnail_url": "urlplaceholder/style_2_photo_number_thumbnail.jpg",
+                      "url": "urlplaceholder/style_2_photo_number.jpg"
+          }
+              ],
+          "skus": {
+                    "37": {
+                          "quantity": 8,
+                          "size": "XS"
+                    },
+                    "38": {
+                          "quantity": 16,
+                          "size": "S"
+                    },
+                    "39": {
+                          "quantity": 17,
+                          "size": "M"
+                    },
+                }
+      }
+  ]
+  }))
+  })
 )
 
 // establish API mocking before all tests
