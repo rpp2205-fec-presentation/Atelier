2c527b4b83dd6f83f04a68c0dde50d06fab00521
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-10-13 09:38:28 -0400

    added pageLoading state, wait until page is loaded to render

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,26 +9,27 @@ const App = () => {
   const [reviews, setReviews] = useState();
   const [metaData, setMetaData] = useState();
   const [ratings, setRatings] = useState({1:0, 2:0, 3:0, 4:0, 5:0});
+  const [pageLoading, setPageLoading] = useState(true);
 
   const getData = () => {
-    axios.get('/reviews', {params: {product_id: product_id, count: 100}})
-      .then(reviews => {setReviews(reviews.data.results)})
-      .catch(err => {console.log(err)});
-    axios.get('/reviews/meta', {params: {product_id: product_id}})
+    const fetchReviews = axios.get('/reviews', {params: {product_id: product_id, count: 100}})
+      .then(reviews => {setReviews(reviews.data.results)});
+    const fetchMeta = axios.get('/reviews/meta', {params: {product_id: product_id}})
       .then(meta => {
         setMetaData(meta.data);
         setRatings(meta.data.ratings);
-      })
-      .catch(err => {console.log(err)});
+      });
+
+      Promise.all([fetchReviews, fetchMeta])
+      .then(() => {setPageLoading(false)})
+      .catch(err => console.log(err));
   }
 
   useEffect(() => {
     getData();
   }, [])
 
-  if (!metaData || !reviews) {
-    return null
-  }
+  if (pageLoading) { return 'Page is Loading'}
 
     return (<div>
       <Overview />
