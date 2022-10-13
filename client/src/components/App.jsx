96adab41c905f91c00f74f39f82a4b17044f0d23
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-10-12 23:31:38 -0400

    setting of state of product id, reviews, reviewMetaData, and ratings are now in App.jsx and passed down to components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Overview from './overview/Overview.jsx';
 import RelatedItems from './relatedItems/RelatedItems.jsx';
 import RatingsReviews from './ratings&reviews/RatingsReviews.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [product_id, setId] = useState(71697);
+  const [reviews, setReviews] = useState();
+  const [metaData, setMetaData] = useState();
+  const [ratings, setRatings] = useState({1:0, 2:0, 3:0, 4:0, 5:0});
+
+  const getData = () => {
+    axios.get('/reviews', {params: {product_id: product_id, count: 100}})
+      .then(reviews => {setReviews(reviews.data.results)})
+      .catch(err => {console.log(err)});
+    axios.get('/reviews/meta', {params: {product_id: product_id}})
+      .then(meta => {
+        setMetaData(meta.data);
+        setRatings(meta.data.ratings);
+      })
+      .catch(err => {console.log(err)});
   }
 
-  render() {
+  useEffect(() => {
+    getData();
+  }, [])
+
+  if (!metaData || !reviews) {
+    return null
+  }
+
     return (<div>
       <Overview />
       <RelatedItems />
-      <RatingsReviews />
+      <RatingsReviews product_id={product_id} reviews={reviews} metaData={metaData} ratings={ratings}/>
     </div>)
-  }
+
 }
 
 export default App;
