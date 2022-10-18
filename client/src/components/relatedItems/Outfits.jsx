46b57727fa6fe7e01918c22239c4049969cfcd5b
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-17 21:10:08 -0700

    render outfits with different action icon

--- client/src/components/relatedItems/Outfits.jsx
+++ client/src/components/relatedItems/Outfits.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ProductCard from './ProductCard.jsx';
+import { faX } from '@fortawesome/free-solid-svg-icons'
+
 
 class Outfits extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productId: 71697,
+      outfits: [71697, 71698, 71699]
+    }
   }
 
   render() {
     return (<div id='outfits'>
-      <h3>My Outfits</h3>
+      <h3>Outfits</h3>
+      {this.state.outfits.map((productId) =>
+        <ProductCard key={productId.toString()} productId={productId} setNewProductId={this.props.setNewProductId} actionButtonIcon={faX}/>)}
     </div>)
   }
 }
