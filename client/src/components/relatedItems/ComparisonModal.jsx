eaa67fd57008b161f7d5b14f0ca57a915829e5e3
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-19 20:08:15 -0700

    begin pulling product data

--- client/src/components/relatedItems/ComparisonModal.jsx
+++ client/src/components/relatedItems/ComparisonModal.jsx
@@ -1,29 +1,61 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
+const axios = require('axios');
+
 
 class ComparisonModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentProductFeatures: [],
+      comparisonProductFeatures: []
+    }
 
     this.generateComparisonInfo = this.generateComparisonInfo.bind(this);
+    this.getProductFeatures = this.getProductFeatures.bind(this);
   }
 
   generateComparisonInfo(currentProductId, comparisonProductId) {
+    this.getProductFeatures(currentProductId, 'currentProductFeatures');
+    this.getProductFeatures(comparisonProductFeatures, 'comparisonProductFeatures');
+
     return (
       <table className='comparison-table'>
-        <tr>
-          <th>{currentProductId}</th>
-          <th>CHECKS</th>
-          <th>{comparisonProductId}</th>
-        </tr>
-        <tr>
-          <td><FontAwesomeIcon icon={faCheck} /></td>
-          <td>Some parameter that I want to compare!</td>
-          <td><FontAwesomeIcon icon={faCheck} /></td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>{currentProductId}</th>
+            <th>CHECKS</th>
+            <th>{comparisonProductId}</th>
+          </tr>
+          <tr>
+            <td><FontAwesomeIcon icon={faCheck} /></td>
+            <td>Some parameter that I want to compare!</td>
+            <td><FontAwesomeIcon icon={faCheck} /></td>
+          </tr>
+        </tbody>
       </table>
     )
+  }
+
+  getProductFeatures(productId, stateParam) {
+    return axios({
+      method: 'get',
+      url: `/products/${productId}`,
+      params: {product_id: productId}
+    })
+    .then(result => {
+      var tempVal = {};
+      tempVal[stateParam] = result.data.features;
+      this.setState({tempVal});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  componentDidUpdate() {
+
   }
 
   render() {
