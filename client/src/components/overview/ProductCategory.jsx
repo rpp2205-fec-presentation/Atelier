225769b39b8fc4cbcec9cc6893cde046a3daf01f
Yunsup Jung <yuun@Yunsups-MBP.attlocal.net>
2022-10-20 22:52:50 -0700

    update some css and style functions

--- client/src/components/overview/ProductCategory.jsx
+++ client/src/components/overview/ProductCategory.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const ProductCategory = ({ category, name }) => {
   return (
-    <div>
+    <section>
       <section id="po-product-categoty" >
         {category}
       </section>
       <section id="po-product-name">
         {name}
       </section>
-    </div>
+    </section>
   )
 }
 
--- client/src/components/overview/ProductStyle.jsx
+++ client/src/components/overview/ProductStyle.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BiCheckCircle } from 'react-icons/bi';
 import getStyleInfoById from "../helpers/getStyleInfoById.js";
 import SelectedProduct from "./SelectedProduct.jsx";
 
@@ -12,6 +13,7 @@ class ProductStyle extends React.Component {
       selectedStyle: '',
     }
     this.getData = this.getData.bind(this);
+    this.stlyeClickhandler = this.stlyeClickhandler.bind(this);
   }
 
   getData(id) {
@@ -29,16 +31,24 @@ class ProductStyle extends React.Component {
           images: newImages,
           defaultStyle: newDefaultStyle,
           selectedStyle: newDefaultStyle
-        })
+        });
       })
+  }
+
+  stlyeClickhandler(e) {
+    const { data, selectedStyle } = this.state;
+    const clickedStyle = e.target.name;
+    this.setState({selectedStyle : data[clickedStyle]});
   }
 
   componentDidMount() {
     this.getData(this.props.id);
   }
 
-  componentWillReceiveProps(nextProps) {
-    this.getData(nextProps.id);
+  componentDidUpdate(prevProps) {
+    if (prevProps.id !== this.props.id) {
+      this.getData(this.props.id);
+    }
   }
 
 
@@ -48,10 +58,17 @@ class ProductStyle extends React.Component {
       <div>
         {<SelectedProduct selectedStyle={selectedStyle} />}
         <div>
-          <h3>Style > {defaultStyle.name}</h3>
+          <h3 id="po-all-style">Style > {selectedStyle.name}</h3>
           {images.map((img, idx) => (
-            <img key={idx} src={img} alt="Style Photo" />
+            <img
+              key={idx}
+              name={idx}
+              className="po-each-style-photo clickable"
+              onClick={this.stlyeClickhandler}
+              src={img}
+              alt="po-each-style-photo" />
           ))}
+          <BiCheckCircle id="tick" />
         </div>
       </div>
     )
