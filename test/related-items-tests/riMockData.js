ab48b8cb3e56e3f5cd1e336fa7c37804b9a83ce1
Sheeva Haghighat <h.sheeva@gmail.com>
2022-10-24 19:11:29 -0700

    use a file with mock data

---
+++ test/related-items-tests/riMockData.js
@@ -0,0 +1,93 @@
+module.exports.productData = {
+  "id": 11,
+  "name": "Air Minis 250",
+  "slogan": "Full court support",
+  "description": "This optimized air cushion pocket reduces impact but keeps a perfect balance underfoot.",
+  "category": "Basketball Shoes",
+  "default_price": "0",
+  "features": [
+    {
+      "feature": "Sole",
+      "value": "Rubber"
+    },
+    {
+      "feature": "Material",
+      "value": "FullControlSkin"
+    },
+  ],
+};
+
+module.exports.ratingData = {
+  "product_id": "2",
+  "ratings": {
+    1: 3,
+    2: 1,
+    3: 1,
+    4: 2,
+    5: 3
+}};
+
+module.exports.styleData =
+{
+  "product_id": "1",
+  "results": [
+    {
+      "style_id": 1,
+      "name": "Forest Green & Black",
+      "original_price": "140",
+      "sale_price": "0",
+      "default?": false,
+      "photos": [
+        {
+          "thumbnail_url": "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+          "url": "urlplaceholder/style_1_photo_number.jpg"
+        },
+        {
+          "thumbnail_url": "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+          "url": "urlplaceholder/style_1_photo_number.jpg"
+        }
+      ],
+      "skus": {
+        "37": {
+          "quantity": 8,
+          "size": "XS"
+        },
+        "38": {
+          "quantity": 16,
+          "size": "S"
+        },
+        "39": {
+          "quantity": 17,
+          "size": "M"
+        },
+      }
+    },
+    {
+      "style_id": 2,
+      "name": "Desert Brown & Tan",
+      "original_price": "140",
+      "sale_price": "0",
+      "default?": true,
+      "photos": [
+        {
+          "thumbnail_url": "urlplaceholder/style_2_photo_number_thumbnail.jpg",
+          "url": "urlplaceholder/style_2_photo_number.jpg"
+        }
+      ],
+      "skus": {
+        "37": {
+          "quantity": 8,
+          "size": "XS"
+        },
+        "38": {
+          "quantity": 16,
+          "size": "S"
+        },
+        "39": {
+          "quantity": 17,
+          "size": "M"
+        },
+      }
+    }
+  ]
+}
